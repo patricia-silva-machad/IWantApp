83b401d00d65af68c12779a9bd3b4fef9d2b72ac
Patricia Machado <88860311+patricia-silva-machad@users.noreply.github.com>
2023-01-22 18:06:18 -0300

    iniciando projeto e criacao/validacao de endpoints

---
+++ src/Categories/CategoryPost.cs
@@ -0,0 +1,31 @@
+﻿using IWantApp.Domain.Products;
+using IWantApp.Infra.Data;
+
+namespace IWantApp.Endpoints.Categories;
+//endpoint tendo so a logica de cadastrar a categoria
+public class CategoryPost {
+    
+    // => ao criar a propriedade template ele ja esta setando o valor "/categories"
+    public static string Template => "/categories";
+    public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
+    public static Delegate Handle => Action;
+
+    public static IResult Action(CategoryRequest categoryRequest, ApplicationDbContext context) 
+        {
+
+        var category = new Category {
+            
+                Name = categoryRequest.Name,
+                CreatedBy = "Test",
+                CreatedOn = DateTime.Now,
+                EditedBy = "Test",
+                EditedOn = DateTime.Now,
+            };
+            context.Categories.Add(category);
+            context.SaveChanges();
+
+            return Results.Created($"/categories/{category.Id}", category.Id);
+
+    }
+
+}
---
+++ src/Categories/CategoryRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace IWantApp.Endpoints.Categories;
+
+public class CategoryRequest{
+
+    public string Name { get; set; }
+
+    public bool Active  {get; set; }
+    
+
+}
