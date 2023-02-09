e33546ce804ef5bc836c2a127c40f750a3b62652
Patricia Machado <88860311+patricia-silva-machad@users.noreply.github.com>
2023-02-08 21:28:19 -0300

    gerando e aplicando migrations

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # IWantApp
+
+
+
+
+
+/*
+*   Claims = atributos que um usuario pode ter. Ex: nome, nacionalidade, idade
+*   Identity = é um sistema de associação que permite adicionar funcionalidade de login ao app.
+*   
+*
+*/
--- src/Infra/Data/ApplicationDbContext.cs
+++ src/Infra/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using Flunt.Notifications;
 using IWantApp.Domain.Products;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace IWantApp.Infra.Data;
 
-public class ApplicationDbContext : DbContext {
+public class ApplicationDbContext : IdentityDbContext<IdentityUser> {
         
     public DbSet<Product> Products { get; set; }
 
@@ -13,6 +15,8 @@ public class ApplicationDbContext : DbContext {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder builder) {
+
+        base.OnModelCreating(builder);
 
         builder.Ignore<Notification>();
         builder.Entity<Product>()
